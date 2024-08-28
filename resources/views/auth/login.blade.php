c50e3c9dfe54c36f294987491784c95bedf2f802
Vishrut Golakiya <101960778+vishrut-golakiya@users.noreply.github.com>
2024-08-28 14:55:12 +0530

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,10 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">{{ __('Login Demo') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+                    <form method="POST" action="{{ route('login demo') }}">
                         @csrf
 
                         <div class="row mb-3">
